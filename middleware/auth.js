65e7e6044e6e0cc78966221510c87e34a4a5174e
mythily-muthu <mythilimuthu18@gmail.com>
2023-03-05 11:11:09 +0530

    Feat: token verification added

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+import { Jwt } from "jsonwebtoken";
+
+
+export let verifyToken = async (req, res, next) => {
+    try {
+        let token = req.header("Authorization")
+
+        if (!token) {
+            return res.status(403).send("access denied")
+        }
+        if (token.startsWith("Bearer ")) {
+            token = token.slice(7, token.length).trimleft();
+        }
+
+        const verified = Jwt.verify(token, process.env.JWT_SECRET)
+        req.user = verified;
+        next();
+
+
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
